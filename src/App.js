c05b3d36a6d26cfbfefeadee2ec271939b8f7858
@damania <fr454807@dal.ca>
2022-06-18 11:15:21 -0300

    added hrefs and navigations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Nav from './navbar/nav.tsx'
-import { Outlet } from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from "./navbar/nav.tsx";
+import { Outlet } from "react-router-dom";
 
-import SignIn from './up/SignIn.js';
-import SignUp from './up/SignUp.js';
-
-import Feed from './Feed/feed.js'
+import SignIn from "./up/SignIn.js";
+import SignUp from "./up/SignUp.js";
+import CreatePost from "./Posts/createPost";
+import Feed from "./Feed/feed.js";
+import UserProfile from "./userprofile/userProfile";
 
 const SidebarLayout = () => (
   <>
@@ -18,21 +19,20 @@ const SidebarLayout = () => (
 );
 function App() {
   return (
-    
-      <Router>
-        <div className='container'>
+    <Router>
+      <div className="container">
         <Routes>
-        <Route element={<SidebarLayout/>}>
-        <Route path="/user/feed" element={<Feed />} />
-
+          <Route element={<SidebarLayout />}>
+            <Route path="/user/feed" element={<Feed />} />
+            <Route path="/user/createpost" element={<CreatePost />} />
+            <Route path="/user/profile" element={<UserProfile />} />
           </Route>
 
           <Route path="/login" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
-        </div>
-      </Router>
-
+      </div>
+    </Router>
   );
 }
 
